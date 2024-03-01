484221e78749afe1841f9f62f5a4ce5525ecd070
Ryan varela <108085059+varelaprogramador@users.noreply.github.com>
2024-02-29 22:02:39 -0300

    Add files via upload

---
+++ Padraosingle/src/com/padrao/Rota.java
@@ -0,0 +1,42 @@
+package com.padrao;
+
+public class Rota {
+	//Crio um objeto para usar quando instanciar
+	private static Rota instance;
+	///
+	String nameAviator;
+	//Construtor para as açoes;
+	public Rota(String nomeiAviator) {
+		
+		
+		if(nomeiAviator!= null){
+			
+			this.nameAviator=nomeiAviator;
+			System.out.printf("criei o aviao :"+nameAviator+"\n");
+			
+		}
+	}
+	
+	
+	//Função para contruir a minhas instancia 
+	
+	public static Rota getInstance(String nameAviator) {
+		
+	//verifico se esta classe nao esta  instanciada
+		
+		if(instance==null) {
+			// caso nao esteja instancio
+			
+			instance=new Rota(nameAviator);
+		
+		}else {
+			// esteja exibo esta mensagem;
+			System.out.printf("\n"+"Não foi possivel colocar o aviao "+nameAviator+" na rota!! "+"\n"+"A rota esta ocupada"+"....");
+		
+		}
+			
+		return instance;
+		
+	}
+
+}
---
+++ Padraosingle/src/com/padrao/Teste.java
@@ -0,0 +1,12 @@
+package com.padrao;
+
+public class Teste {
+	public static void main(String []Args) {
+		Rota aviao1=Rota.getInstance("Airbus");
+		Rota aviao2=Rota.getInstance("Boing");
+		
+		
+		
+	}
+
+}
